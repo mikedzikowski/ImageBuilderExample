d6b6dbce802f6f4bad61218ba1fb51db1f2e3623
Mike Dzikowski <34066455+mikedzikowski@users.noreply.github.com>
2022-08-18 09:56:40 -0400

    Update office.ps1

--- office.ps1
+++ office.ps1
@@ -10,7 +10,7 @@ try {
     Invoke-WebRequest -Uri 'https://<YOUR STORAGE ACCOUNT>.blob.core.usgovcloudapi.net/<YOUR CONTAINER>/setup.zip' -OutFile "Office365.zip"
     Expand-Archive -LiteralPath '.\Office365.zip' -Force
     Set-Location .\Office365
-    Invoke-WebRequest -Uri 'https://<YOUR STORAGE ACCOUNT>.blob.core.usgovcloudapi.net/<YOUR CONTAINER>/configuration-Office365-x64.xml' -OutFile 'OfficeConfig.xml'
+    Invoke-WebRequest -Uri 'https://<YOUR STORAGE ACCOUNT>.blob.core.usgovcloudapi.net/<YOUR CONTAINER>/<YOUR XML>.xml' -OutFile 'OfficeConfig.xml'
     Start-Process -FilePath '.\setup.exe' -ArgumentList "/configure .\OfficeConfig.xml" -WorkingDirectory ".\" -Wait
     write-host 'AIB Customization: Finished Install Office365+Project 2019+Visio2019'
 }
