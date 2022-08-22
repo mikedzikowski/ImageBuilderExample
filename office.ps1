52f9fc8b23af4fa78ad4d426c3513ec5ea3447a5
Mike Dzikowski <34066455+mikedzikowski@users.noreply.github.com>
2022-08-22 09:21:51 -0400

    Update office.ps1

--- office.ps1
+++ office.ps1
@@ -15,5 +15,5 @@ Write-Host "DIRECTORY: $($directory.DirectoryName[0])"
 Write-Host "FILES: $($directory)"
 Invoke-WebRequest -Uri 'https://<STORAGE ACCOUNT>.blob.core.usgovcloudapi.net/<CONTAINER>/<YOUR OFFICE XML>.xml' -OutFile $xmlOutputPath
 Start-Process -FilePath $officeExe -Args "/configure $xmlOutputPath" -PassThru -NoNewWindow -Wait -Verbose
-write-host 'AIB Customization: Finished Install Office365+Project 2019+Visio2019'
+Write-host 'AIB Customization: Finished Install Office365+Project 2019+Visio2019'
 
