d9115bbad97cb64c6f3f23ef4047324a6b2d3efc
Mike Dzikowski <34066455+mikedzikowski@users.noreply.github.com>
2023-07-10 09:55:50 -0400

    Update install-notepad++.ps1

--- install-notepad++.ps1
+++ install-notepad++.ps1
@@ -1,4 +1,3 @@
-mkdir 'c:\temp'
 Invoke-WebRequest 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.2.1/npp.8.2.1.Installer.exe' -OutFile c:\temp\npp.8.2.1.Installer.exe
 Start-Process 'C:\temp\npp.8.2.1.Installer.exe' /S -NoNewWindow -Wait -PassThru
 
